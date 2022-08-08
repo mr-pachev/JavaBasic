9d426ec861cdc8e97e2216466b371fcc2d1875d2
Ivan Pachev <mr.pachev@abv.bg>
2022-08-08 20:23:48 +0300

    ProjectsCreation_07
    Projects Creation.
    Input: George
    4
    Output: The architect George will need 12 hours to complete 4 project/s.

---
+++ Lab/ProjectsCreation_07.java
@@ -0,0 +1,17 @@
+package Lab;
+
+import java.util.Scanner;
+
+public class ProjectsCreation_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int numProjects = Integer.parseInt(scanner.nextLine());
+        int hoursProjects = numProjects * 3;
+
+        System.out.printf("The architect " + "%s" + " will need " + "%s" + " hours to complete " + "%d" + " project/s.",
+                name, hoursProjects, numProjects);
+
+    }
+}
