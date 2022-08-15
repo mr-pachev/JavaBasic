8dfaeec4cb91db18ff2c5dd9be976cec46560447
Ivan Pachev <mr.pachev@abv.bg>
2022-08-15 22:10:38 +0300

    DayOfWeek_01

--- MoreExercises_06/Demo.java
+++ MoreExercises_06/Demo.java
@@ -8,24 +8,17 @@ public class Demo {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        for (int i = 1; i <= (n / 2) + 1; i++) { //върти полавината ред на диаманта включително средния
 
-            // лява част на реда
-            for (int j = i; j <= (n / 2) + 1; j++) { //върти местоположението но * в дадения половин ред
-                if (j <  (n / 2) + 1) {
-                    System.out.print("-");
-                } else {
-                    System.out.print("*");
-                }
+        for (int i = 1; i <= n; i++) {
+            if (i <= (n-1) / 2 || i > (n-1) / 2 && i != ((n-1) / 2) + 1){
+                System.out.print("-");
+            }else{
+                System.out.print("*");
             }
-            if (i > 1) { //допълва до края на полу реда с тирета след *
-                for (int k = ((n / 2) - i); k < (n / 2) - 1; k++) {
-                    System.out.print("-");
-                }
-            }
+        }
 
             System.out.println();
-        }
 
     }
 }
+
