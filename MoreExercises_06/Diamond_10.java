c7ea6a688195621f73d67de806689effff5e4587
Ivan Pachev <mr.pachev@abv.bg>
2022-08-17 22:22:55 +0300

    Diamond_10

--- MoreExercises_06/Diamond_10.java
+++ MoreExercises_06/Diamond_10.java
@@ -8,23 +8,36 @@ public class Diamond_10 {
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        int leftRight = (n - 1) / 2;
+        int leftRight = (n - 1) / 2; //тирета от едната страна на реда
 
-        for (int row = 0; row < n; row++) {
+        for (int row = 1; row <= n - 1; row++) { //цикъл минава през всеки ред от диаманта
+            for (int i = 1; i <= n; i++) { //цикъл да мине през всички индекси на конкретния дадения ред
 
-            for (int i = 0; i <= n; i++) { //цикъл да мине през всички индекси на реда
-
-                if (i <= leftRight - 1 && row != (n / 2) + 1) {
+                if (i <= leftRight) {
                     System.out.print("-");
-                } else if (i == leftRight) {
+                } else if (i == leftRight + 1) {
                     System.out.print("*");
 
-                } else if (i == leftRight + 1 && row > 0 && leftRight >= 0) {
-                    for (int j = 1; j <= row; j++) {
-                        System.out.print("--");
-                        i += 2;
+                } else if (i == leftRight + 2 && row > 1 && leftRight >= 0 && row != n) {
+                    if (row > n / 2) {
+                        for (int j = 1; j <= n - (row + 1); j++) {
+                            System.out.print("--");
+                            if(j % 2 == 0) {
+                                i += 2;
+                            }else {
+                                i++;
+                            }
+                        }
+                    }else {
+                        for (int j = 1; j <= row - 1; j++) {
+                            System.out.print("--");
+                            if(j % 2 == 0) {
+                                i += 2;
+                            }else {
+                                i++;
+                            }
+                        }
                     }
-
                 } else if (i > n - leftRight) {
                     System.out.print("-");
 
@@ -34,7 +47,12 @@ public class Diamond_10 {
 
             }
             System.out.println();
-            leftRight--;
+            if (row <= (n / 2) - 1) {
+                leftRight--;
+            } else {
+                leftRight++;
+            }
+
         }
 
     }
