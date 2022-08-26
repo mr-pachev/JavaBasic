141535a5242adc5857e285c4c188f54a12e0fa4f
Ivan Pachev <mr.pachev@abv.bg>
2022-08-26 15:57:39 +0300

    SafePasswordsGenerator_07

--- MoreExercises_07/SafePasswordsGenerator_07.java
+++ MoreExercises_07/SafePasswordsGenerator_07.java
@@ -8,76 +8,40 @@ public class SafePasswordsGenerator_07 {
 
         int a = Integer.parseInt(scanner.nextLine());
         int b = Integer.parseInt(scanner.nextLine());
-        int numPassCombinations = Integer.parseInt(scanner.nextLine()); //брой допустими комбинаций
-        int counterSymbolA = 0;
-        int counterSymbolB = 0;
+        int maxPassCombinations = Integer.parseInt(scanner.nextLine()); //брой допустими комбинаций
 
-        for (int allSunPasscode = 1; allSunPasscode <= numPassCombinations; allSunPasscode++) { //брояч на генерираните пароли
+        int counterCombination = 0;
+        int firstDigit = 35;
+        int secondDigit = 64;
+        boolean isEnough = false;
 
-            if (35 + counterSymbolA <= 55) { //проверка дали А превишава 55
+        for (int thirdDigit = 1; thirdDigit <= a; thirdDigit++) {
+            for (int forthDigit = 1; forthDigit <= b; forthDigit++) {
+                counterCombination++;
 
-                for (int symbolA = 35 + counterSymbolA; symbolA <= 55; symbolA++) {
-                    char charSymbolA = (char) symbolA;
-
-                    if (64 + counterSymbolB <= 96) { //проверка дали B превишава 96
-                        for (int symbolB = 64 + counterSymbolB; symbolB <= 96; symbolB++) {
-                            char charSymbolB = (char) symbolB;
-
-                            for (int midDigitOne = 1; midDigitOne <= a; midDigitOne++) {
-                                for (int midDigitTwo = 1; midDigitTwo <= b; midDigitTwo++) {
-                                    System.out.printf("%c%c%d%d%c%c", charSymbolA, charSymbolB, midDigitOne, midDigitTwo, charSymbolB, charSymbolA);
-                                    System.out.printf("|");
-                                }
-                            }
-                        }
-                    } else {
-                        for (int symbolB = 64; symbolB <= 96; symbolB++) {
-                            char charSymbolB = (char) symbolB;
-
-                            for (int midDigitOne = 1; midDigitOne <= a; midDigitOne++) {
-                                for (int midDigitTwo = 1; midDigitTwo <= b; midDigitTwo++) {
-                                    System.out.printf("%c%c%d%d%c%c", charSymbolA, charSymbolB, midDigitOne, midDigitTwo, charSymbolB, charSymbolA);
-                                    System.out.printf("|");
-                                }
-                            }
-                        }
-                    }
+                if (firstDigit > 55){
+                    firstDigit = 35;
                 }
-                counterSymbolA += 1;
-                counterSymbolB += 1;
-            } else {
-                counterSymbolA = 1;
-                for (int symbolA = 35; symbolA <= 55; symbolA++) {
-                    char charSymbolA = (char) symbolA;
-
-                    if (64 + counterSymbolB <= 96) { //проверка дали B превишава 96
-                        for (int symbolB = 64 + counterSymbolB; symbolB <= 96; symbolB++) {
-                            char charSymbolB = (char) symbolB;
-
-                            for (int midDigitOne = 1; midDigitOne <= a; midDigitOne++) {
-                                for (int midDigitTwo = 1; midDigitTwo <= b; midDigitTwo++) {
-                                    System.out.printf("%c%c%d%d%c%c", charSymbolA, charSymbolB, midDigitOne, midDigitTwo, charSymbolB, charSymbolA);
-                                }
-                            }
-                        }
-
-                    } else {
-                        counterSymbolB = 1;
-                        for (int symbolB = 64; symbolB <= 96; symbolB++) {
-                            char charSymbolB = (char) symbolB;
-
-                            for (int midDigitOne = 1; midDigitOne <= a; midDigitOne++) {
-                                for (int midDigitTwo = 1; midDigitTwo <= b; midDigitTwo++) {
-                                    System.out.printf("%c%c%d%d%c%c", charSymbolA, charSymbolB, midDigitOne, midDigitTwo, charSymbolB, charSymbolA);
-                                }
-                            }
-                        }
-                    }
-                    counterSymbolA += 1;
-                    counterSymbolB += 1;
+                if (secondDigit > 96){
+                    secondDigit = 64;
                 }
+
+                if(counterCombination > maxPassCombinations){
+                    isEnough = true;
+                    break;
+                }
+
+                System.out.printf("%c%c%d%d%c%c|", firstDigit, secondDigit, thirdDigit, forthDigit,secondDigit, firstDigit);
+                firstDigit++;
+                secondDigit++;
+
+            }
+
+            if(isEnough){
+                break;
             }
         }
+
 
 
     }
