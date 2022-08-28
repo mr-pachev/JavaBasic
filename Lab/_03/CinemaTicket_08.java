14484f43a4e45a789b92c64775a00aee1b63d255
Ivan Pachev <mr.pachev@abv.bg>
2022-08-28 20:28:36 +0300

    CinemaTicket_08

---
+++ Lab/_03/CinemaTicket_08.java
@@ -0,0 +1,24 @@
+package Lab._03;
+
+import java.util.Scanner;
+
+public class CinemaTicket_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String dayOfWeek = scanner.nextLine();
+
+        if (dayOfWeek.equals("Monday") ||
+                dayOfWeek.equals("Tuesday") ||
+                dayOfWeek.equals("Friday")) {
+            System.out.println(12);
+        } else if ((dayOfWeek.equals("Wednesday") ||
+                (dayOfWeek.equals("Thursday")))) {
+            System.out.println(14);
+        } else if ((dayOfWeek.equals("Saturday") ||
+                (dayOfWeek.equals("Sunday")))) {
+            System.out.println(16);
+        }
+    }
+}
+
