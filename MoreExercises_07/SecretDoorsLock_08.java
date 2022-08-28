7ff9bb29e0070c5cf30a4579c1c010869a1dc358
Ivan Pachev <mr.pachev@abv.bg>
2022-08-28 20:25:22 +0300

    SecretDoorsLock_08

--- MoreExercises_07/SecretDoorsLock_08.java
+++ MoreExercises_07/SecretDoorsLock_08.java
@@ -10,20 +10,18 @@ public class SecretDoorsLock_08 {
         int secondDigit = Integer.parseInt(scanner.nextLine());
         int firstDigit = Integer.parseInt(scanner.nextLine());
 
-        for (int hundredshundreds = 1; hundredshundreds <= thirdDigit; hundredshundreds++) { //цикъл за стотиците
-            if (hundredshundreds % 2 == 0) { //филтър само за четните стотици
+        for (int hundreds = 1; hundreds <= thirdDigit; hundreds++) { //цикъл за стотиците
+            if (hundreds % 2 == 0) { //филтър само за четните стотици
 
                 for (int dozens = 1; dozens <= secondDigit; dozens++) { //цикъл за десетиците
-                    if (dozens / 2 == 1 || dozens % 3 == 0 || dozens % 5 == 0 || dozens % 7 == 0) { //филтър за десетицит
+                    if (dozens == 2 || dozens == 3 || dozens == 5 || dozens == 7) { //филтър за десетиците - да е просто число
 
                         for (int units = 1; units <= firstDigit; units++) { //цикъл за едениците
                             if (units % 2 == 0) { //филтър за едениците
 
-                                System.out.printf("%d%d%d%n", hundredshundreds, dozens, units);
+                                System.out.printf("%d %d %d%n", hundreds, dozens, units);
                             }
-
                         }
-
                     }
                 }
             }
