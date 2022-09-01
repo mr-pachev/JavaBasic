14a4a5fb421f02e2762ce4ee6675684a4db036d0
Ivan Pachev <mr.pachev@abv.bg>
2022-09-01 17:00:01 +0300

    Renovation_04

--- Exam_01/Renovation_04.java
+++ Exam_01/Renovation_04.java
@@ -10,33 +10,33 @@ public class Renovation_04 {
         int width = Integer.parseInt(scanner.nextLine());
         int areaNoPainPercent = Integer.parseInt(scanner.nextLine()); //процент от общата площ,която няма да се боядисва
         String incoming = scanner.nextLine();
-        double paintArea = 0; //текуща боядисана площ
+
+        int paintArea = 0; //текуща боядисана площ
         boolean isPaint = false;
-        double allPaintArea = 0; //общо боядисана площ
+        int allPaintArea = 0; //общо боядисана площ
 
         //изчисление площа за боядисване
-        double areaForPain = height * width * 4;
-        double areaNoPain = areaForPain * (areaNoPainPercent * 1.0/ 100); //площ, която няма да се боядисва
-        areaForPain = Math.ceil(areaForPain - areaNoPain); //общо площ за боядисване на една стая с 4 стени
+        int areaForPain = height * width * 4;
+        int areaNoPain = areaForPain * areaNoPainPercent / 100; //площ, която няма да се боядисва
+        areaForPain = (int) Math.ceil(areaForPain - areaNoPain); //общо площ за боядисване на една стая с 4 стени
 
-        while (!incoming.equals("Tired!") && (allPaintArea < areaForPain)){
-            paintArea = Double.parseDouble(incoming);
+        while (!incoming.equals("Tired!")){
+            paintArea = Integer.parseInt(incoming);
 
             allPaintArea += paintArea;
 
-            if (areaForPain < allPaintArea){ //проверка боядисана площ да не е по-голяма от площа за боядисване
+            if (areaForPain <= allPaintArea){ //проверка боядисаната площ да не е по-голяма от площа за боядисване
                 isPaint = true;
-            }
-
-            if (!isPaint) {
+                break;
+            }else {
                 incoming = scanner.nextLine();
             }
         }
         if (!isPaint){ //проверка дали се е изморил преди да боядиса всички стени
-            System.out.printf("%.0f quadratic m left.%n", areaForPain - allPaintArea);
+            System.out.printf("%d quadratic m left.%n", areaForPain - allPaintArea);
         }else { //когато са боядисани всички стени
             if (allPaintArea - areaForPain > 0){ //проверка дали е останала боя
-                System.out.printf("All walls are painted and you have %.0f l paint left!%n", allPaintArea - areaForPain);
+                System.out.printf("All walls are painted and you have %d l paint left!%n", allPaintArea - areaForPain);
             }else {
                 System.out.println("All walls are painted! Great job, Pesho!");
             }
