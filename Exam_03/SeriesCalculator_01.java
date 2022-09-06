590583ac64512ca4e5664064c8909ab2e3f53567
Ivan Pachev <mr.pachev@abv.bg>
2022-09-06 21:08:06 +0300

    InvalidNumber_10

---
+++ Exam_03/SeriesCalculator_01.java
@@ -0,0 +1,12 @@
+package Exam_03;
+
+import java.util.Scanner;
+
+public class SeriesCalculator_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+    }
+}
