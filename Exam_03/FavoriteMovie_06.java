62abcb05d16b7fd7e403a6dc4eb6176df3b0cf44
Ivan Pachev <mr.pachev@abv.bg>
2022-09-12 22:12:46 +0300

    FavoriteMovie_06

--- Exam_03/FavoriteMovie_06.java
+++ Exam_03/FavoriteMovie_06.java
@@ -7,44 +7,44 @@ public class FavoriteMovie_06 {
         Scanner scanner = new Scanner(System.in);
 
         String input = scanner.nextLine();
-        String movieName = ""; //име на филм
 
-        int counterMovie =0; //брояч на филмите
+        String movieName = ""; //име на филм
+        int counterMovie = 0/брояч на филмите
         int bestMoviePoints = 0; //филма с най-много точки до момента
         String bestMovieName = "";
 
-        while (!input.equals("STOP")){
-            int currentPointsCounter = 0; //брояч на точките за текущия филм
+        while (!input.equals("STOP") || counterMovie <= 7) {
             movieName = input;
             counterMovie++;
 
-            if(counterMovie <= 7){
+            if (counterMovie <= 7) { //проверка броя на филмите да не надвишава 7
+                int currentPointsCounter = 0; //брояч на точките за текущия филм
 
-                for (int indexMovieName = 0; indexMovieName < movieName.length(); indexMovieName++) {
+                for (int indexMovieName = 0; indexMovieName < movieName.length(); indexMovieName++) { //цикъл за обхождане на всяка буква от името на филма
                     char currentIndex = movieName.charAt(indexMovieName); //текуща буква от името на филма
 
-                    // проверка дали буквата е малка или голяма
-                    if(currentIndex >= 97 && currentIndex <= 122){ //малка буква
+                    if (currentIndex >= 97 && currentIndex <= 122) { //малка буква
                         currentPointsCounter += currentIndex - (2 * movieName.length());
-                    }else if ( currentIndex >= 65 && currentIndex <= 90){ //голяма буква
+                    } else if (currentIndex >= 65 && currentIndex <= 90) { //голяма буква
                         currentPointsCounter += currentIndex - movieName.length();
-                    }else {
+                    } else { //всеки друг символ
                         currentPointsCounter += currentIndex;
                     }
 
                 }
 
-                if (currentPointsCounter >= bestMoviePoints){
+                if (currentPointsCounter >= bestMoviePoints) { //проверка за най-добър филмо по точки
                     bestMoviePoints = currentPointsCounter;
                     bestMovieName = movieName;
                 }
 
-            }else {
+                input = scanner.nextLine();
+
+            } else {
                 System.out.println("The limit is reached.");
                 break;
             }
 
-            input = scanner.nextLine();
         }
         System.out.printf("The best movie for you is %s with %d ASCII sum.", bestMovieName, bestMoviePoints);
 
