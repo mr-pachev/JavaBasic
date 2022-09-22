14b5d1d14fe3a5d360ed4208bd9dff21c16f487b
Ivan Pachev <mr.pachev@abv.bg>
2022-09-22 10:32:52 +0300

    HighJump_06

--- Exam_05/HighJump_06.java
+++ Exam_05/HighJump_06.java
@@ -7,37 +7,36 @@ public class HighJump_06 {
         Scanner scanner = new Scanner(System.in);
 
         int targetHeight = Integer.parseInt(scanner.nextLine()); //желана височина
-        int startHeight = targetHeight - 30; //стартова височина
-
-        int currentTry = Integer.parseInt(scanner.nextLine()); //текущ опит
+        int lastTry = 0; //последния скок
         int countUnsuccessfulTry = 0; //брояч на неуспешните опити
-        int counterTry = 1; //брояч на скоковете
-
+        int counterTry = 0; //брояч на скоковете
         boolean isSuccess = true;
 
-        while (currentTry <= targetHeight) {
+        for (int startTry = targetHeight - 30; startTry <= targetHeight; startTry += 5) { //върти цикъл до достигане на зададената височина
+            lastTry = startTry;
 
-            if (currentTry > startHeight) { //проверка дали опита е успешен
-                countUnsuccessfulTry = 0;
-                startHeight += 5;
-            } else {
-                countUnsuccessfulTry++;
+            for (int i = 1; i <= 3; i++) { //три опита за скок
+
+                int currentTry = Integer.parseInt(scanner.nextLine()); //текущ опит
+                counterTry++;
+
+                if (currentTry <= startTry) { //проверка дали скока е неуспешен
+                    countUnsuccessfulTry++;
+                } else {
+                    countUnsuccessfulTry = 0;
+                    break;
+                }
             }
 
-            if (countUnsuccessfulTry == 3) { //проверка дали неуспешните опити са повече от 3
+            if (countUnsuccessfulTry == 3) { //проверка дали има три неуспешни опита
                 isSuccess = false;
                 break;
             }
-
-            currentTry = Integer.parseInt(scanner.nextLine());
-            counterTry++;
-
         }
-
         if (!isSuccess) {
-            System.out.printf("Tihomir failed at %dcm after %d jumps.", startHeight, counterTry);
+            System.out.printf("Tihomir failed at %dcm after %d jumps.", lastTry, counterTry);
         } else {
-            System.out.printf("Tihomir succeeded, he jumped over %dcm after %d jumps.", startHeight, counterTry);
+            System.out.printf("Tihomir succeeded, he jumped over %dcm after %d jumps.", lastTry, counterTry);
         }
 
     }
