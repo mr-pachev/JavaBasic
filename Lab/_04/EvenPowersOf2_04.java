070c8788401924c3045daac7c1fb88ae834885e9
Ivan Pachev <mr.pachev@abv.bg>
2022-09-22 21:11:43 +0300

    EvenPowersOf2_04

---
+++ Lab/_04/EvenPowersOf2_04.java
@@ -0,0 +1,16 @@
+package Lab._04;
+
+import java.util.Scanner;
+
+public class EvenPowersOf2_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int num = 1;
+        for (int i = 0; i <= n; i += 2 ){
+            System.out.println(num);
+            num = num  * 2 * 2;
+        }
+    }
+}
