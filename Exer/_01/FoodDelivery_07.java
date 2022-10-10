051b7a8cd6b3695fa978f33f51b4de5c6602fc7c
Ivan Pachev <mr.pachev@abv.bg>
2022-10-10 19:58:42 +0300

    FoodDelivery_07

---
+++ Exer/_01/FoodDelivery_07.java
@@ -0,0 +1,27 @@
+package Exer._01;
+
+import java.util.Scanner;
+
+public class FoodDelivery_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int chicken = Integer.parseInt(scanner.nextLine());
+        int fish = Integer.parseInt(scanner.nextLine());
+        int vegan = Integer.parseInt(scanner.nextLine());
+//        •	Пилешко меню –  10.35 лв.
+//        •	Меню с риба – 12.40 лв.
+//        •	Вегетарианско меню  – 8.15 лв.
+
+        double chickenCount = chicken * 10.35;
+        double fishCount = fish * 12.40;
+        double veganCount = vegan * 8.15;
+        double mealCount = chickenCount + fishCount + veganCount;
+        double dessert = mealCount * 0.20;
+        double orderCount = mealCount + dessert + 2.50;
+
+        System.out.println(orderCount);
+
+
+    }
+}
