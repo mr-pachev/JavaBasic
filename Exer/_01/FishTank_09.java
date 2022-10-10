b8a8d37ec57462f6341c1503c9823b3af301dcd7
Ivan Pachev <mr.pachev@abv.bg>
2022-10-10 20:00:18 +0300

    FishTank_09

---
+++ Exer/_01/FishTank_09.java
@@ -0,0 +1,20 @@
+package Exer._01;
+
+import java.util.Scanner;
+
+public class FishTank_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double lenght = Double.parseDouble(scanner.nextLine());
+        double width = Double.parseDouble(scanner.nextLine());
+        double height = Double.parseDouble(scanner.nextLine());
+        double content = Double.parseDouble(scanner.nextLine());
+
+        double allVolume = (lenght * width * height) / 1000;
+        double volume = allVolume - (allVolume * content / 100);
+
+        System.out.println(volume);
+
+    }
+}
