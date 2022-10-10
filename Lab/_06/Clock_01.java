90d3150c31df0100e4355796d2f76e1a7dae0deb
Ivan Pachev <mr.pachev@abv.bg>
2022-10-10 19:34:49 +0300

    Clock_01

---
+++ Lab/_06/Clock_01.java
@@ -0,0 +1,12 @@
+package Lab._06;
+
+public class Clock_01 {
+    public static void main(String[] args) {
+
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m <= 59; m++) {
+                System.out.printf("%d:%d%n", h, m);
+            }
+        }
+    }
+}
