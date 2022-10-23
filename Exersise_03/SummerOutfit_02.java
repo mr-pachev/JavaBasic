5e51b5b43b84a8194b2f2cf0ba7be5d8b2cc5f92
Ivan Pachev <mr.pachev@abv.bg>
2022-10-23 21:44:12 +0300

    Create SummerOutfit_02.java

---
+++ Exersise_03/SummerOutfit_02.java
@@ -0,0 +1,60 @@
+package Exersise_03;
+
+import java.util.Scanner;
+
+public class SummerOutfit_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int degrees = Integer.parseInt(scanner.nextLine());
+        String partOfTheDay = scanner.nextLine();
+        String outfit = "";
+        String shoes = "";
+
+        if (degrees >= 10 && degrees <=42) {
+
+            switch (partOfTheDay) {
+                case "Morning":
+                    if (degrees >= 10 && degrees <= 18) {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                    } else if (degrees > 18 && degrees <= 24) {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    } else if (degrees >= 25) {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    break;
+
+                case "Afternoon":
+                    if (degrees >= 10 && degrees <= 18) {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    } else if (degrees > 18 && degrees <= 24) {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    } else if (degrees >= 25) {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+                    break;
+
+                case "Evening":
+                    if (degrees >= 10 && degrees <= 18) {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    } else if (degrees > 18 && degrees <= 24) {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    } else if (degrees >= 25) {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    break;
+            }
+
+            System.out.printf("It's %d degrees, get your %s and %s.", degrees, outfit, shoes);
+        }
+    }
+}
